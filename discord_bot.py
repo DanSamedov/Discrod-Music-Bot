afedc49f5db8055cc799edc8a0c61d52435ea47a
Danylo Samedov <danasamedov@gmail.com>
2024-11-08 22:22:55 +0200

    Add ability to play audio by keyword

--- discord_bot.py
+++ discord_bot.py
@@ -18,6 +18,7 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 songs_queue = deque()
 looping = False
 
+
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
@@ -29,18 +30,19 @@ async def oleg_joins():
 
 
 @bot.command()
-async def play(ctx, arg):
+async def play(ctx, *, arg):
     if ctx.author.voice:
         channel = ctx.author.voice.channel
         voice = ctx.guild.voice_client
 
         if voice is None:
             voice = await channel.connect()
-            await ctx.send(f"Joined {channel}")
+            await ctx.send(f"Joined `{channel}`")
         
+        _, song_title = await get_stream_url(arg)
         if voice.is_playing():
             queue_song(arg)
-            await ctx.send(f"Added to queue: {arg}")
+            await ctx.send(f"Added to queue: `{song_title}`")
         else:
             queue_song(arg)
             await play_next(ctx)
@@ -70,14 +72,14 @@ async def stfu(ctx):
 
     if ctx.voice_client is not None:
         songs_queue.clear()
-        await ctx.send(f"Bot has left the voice channel: {ctx.voice_client.channel}")
+        await ctx.send(f"Bot has left the voice channel: `{ctx.voice_client.channel}`")
         await ctx.voice_client.disconnect()
     else:
         await ctx.send("Bot is not in a voice channel.")
 
 
 @bot.command()
-async def loop(ctx, arg):
+async def loop(ctx, *, arg):
     global looping
     looping = True
 
@@ -116,7 +118,7 @@ def queue_song(arg):
 @bot.command()
 async def print_queue(ctx):
     queue_list = list(songs_queue)
-    await ctx.send(f"List of songs: {queue_list}")
+    await ctx.send(f"List of songs: `{queue_list}`")
 
 
 async def play_next(ctx):
@@ -129,9 +131,9 @@ async def play_next(ctx):
 async def skip(ctx):
     global looping
     looping = False
-
+    
     ctx.guild.voice_client.stop()
-    await ctx.send("Done! Playing next song")
+    await ctx.send(f"Skipping current song...")
     play_next(ctx)
 
 
@@ -146,35 +148,56 @@ async def clear(ctx):
 @bot.command()
 async def play_audio(ctx, arg):
     try:
-        stream_url = await get_stream_url(arg)
+        stream_url, song_title = await get_stream_url(arg)
+        voice = ctx.guild.voice_client
+
         if stream_url:
             source = discord.FFmpegPCMAudio(stream_url)
             
             def after_playing(error):
+                if error:
+                    print(f"Error while playing: `{error}`")
                 coro = play_next(ctx)
                 asyncio.run_coroutine_threadsafe(coro, bot.loop)
             
-            ctx.guild.voice_client.play(source, after=after_playing)
-            await ctx.send(f"Now playing: {arg}")
+            if voice.is_playing():
+                voice.stop()
+
+            voice.play(source, after=after_playing)
+            await ctx.send(f"Now playing: `{song_title}`")
         
         else:
             await ctx.send("Could not retrieve audio stream.")
     except Exception as e:
-        await ctx.send(f"An error occurred: {e}")
+        await ctx.send(f"An error occurred: `{e}`")
 
 
 async def get_stream_url(url):
     try:
         if not (url.startswith("http://") or url.startswith("https://")):
             url = f"ytsearch:{url}"
-        
-        ydl_opts = {'format': 'bestaudio'}
+
+        ydl_opts = {
+            'format': 'bestaudio',
+            'noplaylist': True,
+            'quiet': True,
+            'no_warnings': True,
+            'default_search': 'ytsearch',
+        }
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
-            stream_url = info['url']
-        return stream_url
+
+            if 'entries' in info:
+                info = info['entries'][0]
+
+            song_title = info.get('title')
+            stream_url = info.get('url')
+
+        return stream_url, song_title
+        
     except Exception as e:
-        print(f"Error getting stream URL: {e}")
+        print(f"Error getting stream URL: `{e}`")
         return None
 
 
